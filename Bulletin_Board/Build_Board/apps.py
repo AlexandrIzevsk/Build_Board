8569b49c318cb9cb5f8206c997c39cb7dcc73948
AlexandrIzevsk <sasha-sarov-333@rambler.ru>
2025-01-22 06:37:15 +0400

    Build_Board: create, for help with static

---
+++ Bulletin_Board/Build_Board/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BuildBoardConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Build_Board'
---
+++ Bulletin_Board/Build_Board/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
+
+# Create your models here.
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='users')
+
+
+class Advert(models.Model):
+    tanks = 'Tk'
+    hills = 'Hl'
+    dd = 'DD'
+    merchants = 'Mr'
+    gild_masters = 'Gm'
+    kvest_givers = 'Kg'
+    blacksmiths = 'Bm'
+    tanners = 'Tn'
+    potion_makers = 'Pm'
+    spellmasters = 'Sm'
+
+    CHOICES = [
+        (tanks, 'Танки'),
+        (hills, 'Хиллы'),
+        (dd, 'ДД'),
+        (merchants, 'Торговцы'),
+        (gild_masters, 'Гилдмастера'),
+        (kvest_givers, 'Квестгиверы'),
+        (blacksmiths, 'Кузнецы'),
+        (tanners, 'Кожевники'),
+        (potion_makers, 'Зельевары'),
+        (spellmasters, 'Мастера заклинаний')
+    ]
+
+    author = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    choice = models.CharField(max_length=2, choices=CHOICES, default=tanks)
+    time_in = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=128)
+    content = HTMLField()
+
+    def __str__(self):
+        return f'{self.title} - {self.preview()}'
+
+    def preview(self):
+        return self.content[0:123] + '...'
+
+
+class Feedback(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    advert = models.ForeignKey(Advert, on_delete=models.CASCADE)
+    time_in = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+    accept = models.BooleanField(default=False)
